91a85d523fd2cf6bc35f922e4c4638eb9c3f45f9
Muhammet <eem_muhammed@hotmail.com>
2023-03-11 14:48:11 +0300

    enabled swagger release mode

--- src/Webhooks.Sample/Startup.cs
+++ src/Webhooks.Sample/Startup.cs
@@ -56,12 +56,13 @@ namespace Webhooks.Sample
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(
-                    c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Webhooks.Sample v1"));
             }
 
-          //  app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Webhooks.Sample v1"));
+
+            //  app.UseHttpsRedirection();
 
             app.UseRouting();
 
